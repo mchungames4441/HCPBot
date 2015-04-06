8a8728b8f3f2fc70d8a913d7368628856770709c
mchungames4441 <l.souchiere@gmail.com>
2015-04-06 14:25:13 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -47,15 +47,15 @@
             }
         };
         
-        bot.commands.baconCommand = {
-            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.dreamCommand = {
+            command: 'whoisdream',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Bacon!!!");
+                    API.sendChat("/me SexyDream, or Dream, is a magical flying unicorn that looks over the Earth.");
                 }
             }
         };
