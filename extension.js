10592848a8e019f4263222830f12e0f13dd4fc19
mchungames4441 <l.souchiere@gmail.com>
2015-04-06 16:36:53 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -73,7 +73,7 @@
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
-        chatLink: "https://rawgit.com/" + fork + "/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/" + fork + "/HCPBot/master/en.json",
         maximumAfk: 120,
         afkRemoval: false,
         maximumDc: 15,
