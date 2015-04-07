18f8dae1d5609910eca4f0d6f1412fde60caec19
mchungames4441 <l.souchiere@gmail.com>
2015-04-07 09:54:05 -0400

    Added command
    Added !whoissexydream, which is longer version of !whoisdream.

--- extension.js
+++ extension.js
@@ -59,6 +59,19 @@
                 }
             }
         };
+        
+        bot.commands.sdreamCommand = {
+            command: 'whoissexydream',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me SexyDream, or Dream, is a magical flying unicorn who watches over the Earth and everyone in it. Dream really likes kittens.");
+                }
+            }
+        };
 
         //Load the chat package again to account for any changes
         bot.loadChat();
