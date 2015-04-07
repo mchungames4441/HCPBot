05788edecfd63c943334d00a17d44cf3ae7bd583
mchungames4441 <l.souchiere@gmail.com>
2015-04-07 15:16:13 -0400

    Fixed language again
    I hope it works this time...

--- extension.js
+++ extension.js
@@ -112,7 +112,7 @@
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
-        chatLink: "https://rawgit.com/mchungames4441/basicBot/master/lang/en.json",
+        chatLink: "https://raw.githubusercontent.com/mchungames4441/basicBot/master/lang/en.json",
         maximumAfk: 120,
         afkRemoval: false,
         maximumDc: 15,
